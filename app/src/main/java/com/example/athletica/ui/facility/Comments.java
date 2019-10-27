235c4105ecfd43f9075a1d4a2b0d093e96e81c96
nomifaz <nomi.faz@gmail.com>
2019-10-27 16:15:20 +0800

    Ui updates

---
+++ app/src/main/java/com/example/athletica/ui/facility/Comments.java
@@ -0,0 +1,26 @@
+package com.example.athletica.ui.facility;
+
+public class Comments{
+
+        private String userID;
+        //private String facilityID;
+        private String commentContent;
+
+        public Comments(){
+
+        }
+
+        public Comments(String userID,  String commentContent) {
+            this.userID = userID;
+            // this.facilityID=facilityID;
+            this.commentContent = commentContent;
+        }
+
+        public String getUserID() {
+            return userID;
+        }
+
+        public String getCommentContent() {
+            return commentContent;
+        }
+}
---
+++ app/src/main/java/com/example/athletica/ui/facility/Ratings.java
@@ -0,0 +1,46 @@
+package com.example.athletica.ui.facility;
+
+public class Ratings {
+    //private String ratingID;
+    //private String facilityID;
+    private float ratingContent;
+    private float five = 0;
+    private float fourpfive = 0;
+    private float four = 0;
+    private float threepfive = 0;
+    private float three = 0;
+    private float twopfive = 0;
+    private float two = 0;
+    private float onepfive = 0;
+    private float one = 0;
+    private float pfive = 0;
+    private float zero = 0;
+
+
+    public Ratings() {
+    }
+
+
+    public Ratings(int num) {
+        five += num;
+    }
+    public float addrating(float ratingContent){
+        switch((int)(10*ratingContent)){
+            case 50:
+                five++;
+                break;
+            case 45:
+                fourpfive++;
+                break;
+
+        }
+        return five;
+    }
+    public float getFive() {
+        return five;
+    }
+
+    public float getRatingContent() {
+        return ratingContent;
+    }
+}
